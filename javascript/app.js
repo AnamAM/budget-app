338c522c3b1a5c2551c2f1ee2f6fc4228dc26455
Anam Mohammad <m.anam0603@gmail.com>
2019-07-17 17:31:03 -0500

    functions created to set up event listeners and also to call when new item gets added

--- javascript/app.js
+++ javascript/app.js
@@ -1,9 +1,8 @@
 // module that handles budget data 
 // module patterns return an object containing all of the functions that we want public, bascially the functions that we want to give to the outside scope access to
+
 // var budgetController = (function() {
-
 //     var x = 23;
-
 //     // private add()
 //     var add = function(a) {
 //         return x + a;
@@ -13,19 +12,15 @@
 //             return (add(b));
 //         }
 //     }
-
 // })();
 
 // // module that takes care of the UI
 // var UIController = (function() {
-
 // })();
 
 // // modules are function expressions and you can pass arguments into them
 // var controller = (function(budgetCtrl, UTCtrl) {
-
 //     var z = budgetController.publicTest(5);
-
 //     return {
 //         anotherPublic: function() {
 //             console.log(z);
@@ -46,17 +41,23 @@ var UIController = (function () {
     var DOMstrings = {
         inputType: '.add__type',
         inputDescription: '.add__description',
-        inputValue: '.add__value'
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
     };
 
     // public function/method to use in the other controller that will have to be in the object that the IIFE function will return
     return {
         getInput: function () {
             return {
-                type: document.querySelector(DOMstrings.inputType).value, // will be either inc (income) or exp (expense)
+                // will be either inc (income) or exp (expense)
+                type: document.querySelector(DOMstrings.inputType).value,
                 description: document.querySelector(DOMstrings.inputDescription).value,
                 value: document.querySelector(DOMstrings.inputValue).value
             };
+        },
+        // returning private DOMstrings into the public method; exposing DOMstrings object
+        getDOMStrings: function () {
+            return DOMstrings;
         }
     }
 })();
@@ -64,6 +65,22 @@ var UIController = (function () {
 // global app controller
 // set up event listener for the input button -- it is the central place where you can decide where you want to control what happens upon each event and then delegate these tasks to the other controllers 
 var controller = (function (budgetCtrl, UICtrl) {
+
+    var setupEventListeners = function () {
+        var DOM = UICtrl.getDOMStrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click', crtlAddItem);
+        // console.log('button was clicked');
+
+        // event listener for global doc
+        document.addEventListener('keypress', function (event) {
+            // console.log(event);
+            if (event.keyCode === 13 || event.which === 13) {
+                // console.log('ENTER was pressed');
+                crtlAddItem();
+            }
+        })
+    }
 
     var crtlAddItem = function () {
 
@@ -82,16 +99,6 @@ var controller = (function (budgetCtrl, UICtrl) {
         // console.log('it works');
     }
 
-    document.querySelector('.add__btn').addEventListener('click', crtlAddItem);
-    // console.log('button was clicked');
-
-    // event listener for global doc
-    document.addEventListener('keypress', function (event) {
-        // console.log(event);
-        if (event.keyCode === 13 || event.which === 13) {
-            // console.log('ENTER was pressed');
-            crtlAddItem();
-        }
-    })
-
 })(budgetController, UIController);
+
+

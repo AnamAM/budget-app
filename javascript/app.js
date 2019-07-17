cb770b7db690c68a0ec165d40cae5d6288a52a6d
Anam Mohammad <m.anam0603@gmail.com>
2019-07-16 23:26:40 -0500

    user input works through controller being called correctly

--- javascript/app.js
+++ javascript/app.js
@@ -42,17 +42,27 @@ var budgetController = (function () {
 
 // UI controller
 var UIController = (function () {
-
+    // public function/method to use in the other controller that will have to be in the object that the IIFE function will return
+    return {
+        getInput: function () {
+            return {
+                type: document.querySelector('.add__type').value, // will be either inc (income) or exp (expense)
+                description: document.querySelector('.add__description').value,
+                value: document.querySelector('.add__value').value
+            };
+        }
+    }
 })();
 
 // global app controller
 // set up event listener for the input button -- it is the central place where you can decide where you want to control what happens upon each event and then delegate these tasks to the other controllers 
-var controller = (function (budgetCtrl, UTCtrl) {
+var controller = (function (budgetCtrl, UICtrl) {
 
-    document.querySelector('.add__btn').addEventListener('click', function() {
-        // console.log('button was clicked');
+    var crtlAddItem = function () {
 
         // 1. get the field input data
+        var input = UICtrl.getInput();
+        console.log(input);
 
         // 2. add the item to the budget controller
 
@@ -61,15 +71,20 @@ var controller = (function (budgetCtrl, UTCtrl) {
         // 4. calculate the budget 
 
         // 5. display the budget on the UI 
-    })
+
+        // console.log('it works');
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', crtlAddItem);
+    // console.log('button was clicked');
 
     // event listener for global doc
-    document.addEventListener('keypress', function(event) {
+    document.addEventListener('keypress', function (event) {
         // console.log(event);
         if (event.keyCode === 13 || event.which === 13) {
-            console.log('ENTER was pressed');
+            // console.log('ENTER was pressed');
+            crtlAddItem();
         }
-
     })
 
 })(budgetController, UIController);

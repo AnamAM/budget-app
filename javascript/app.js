d9959879fe58d6132b235499e79f6e17f7e0896b
Anam Mohammad <m.anam0603@gmail.com>
2019-07-23 21:11:53 -0500

    created a deleteItem() in budgetController() which successfully removes an item from the data structure

--- javascript/app.js
+++ javascript/app.js
@@ -66,6 +66,20 @@ var budgetController = (function () {
             return newItem;
         },
 
+        deleteItem: function (type, id) {
+            var ids, index;
+
+            ids = data.allItems[type].map(function (current) {
+                return current.id;
+            });
+            index = ids.indexOf(id);
+
+
+            if (index !== -1) {
+                data.allItems[type].splice(index, 1);
+            }
+        },
+
         calculateBudget: function () {
             // calculate total income and expenses 
             calculateTotal('exp');
@@ -201,10 +215,9 @@ var controller = (function (budgetCtrl, UICtrl) {
                 crtlAddItem();
             }
         });
-        // event delegation for deleting items
+        // event delegation started to delete incomes and expenses; used container class element primarily for this reason and did DOM traversing in ctrlDeleteItem function to move up to the parent element 
         document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
     }
-
 
 
     var updateBudget = function () {
@@ -258,9 +271,10 @@ var controller = (function (budgetCtrl, UICtrl) {
             // inc-1
             splitID = itemID.split('-');
             type = splitID[0];
-            ID = splitID[1];
+            ID = parseInt(splitID[1]);
 
             // 1. delete the item from the data structure 
+            budgetCtrl.deleteItem(type, ID);
 
             // 2. delete the item from the UI 
 

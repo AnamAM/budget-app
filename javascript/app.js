00a332eaa1f185d8ecd5fb3ffb01bf258a36c5c1
Anam Mohammad <m.anam0603@gmail.com>
2019-07-24 00:17:25 -0500

    created a deleteListItem() to remove items from the UI

--- javascript/app.js
+++ javascript/app.js
@@ -162,6 +162,12 @@ var UIController = (function () {
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
 
+        deleteListItem: function(selectorID) {
+            var el = document.getElementById(selectorID);
+            el.parentNode.removeChild(el);
+
+        },
+
         clearFields: function () {
             var fields, fieldsArr;
 
@@ -277,6 +283,7 @@ var controller = (function (budgetCtrl, UICtrl) {
             budgetCtrl.deleteItem(type, ID);
 
             // 2. delete the item from the UI 
+            UICtrl.deleteListItem(itemID);
 
             // 3. update and show the new budget 
         }
